68c610f607996a6cfe834a204ea8f3303253b778
AlexeyDer <hottelmich@gmail.com>
2019-11-27 12:56:04 +0700

    Done from DB to CSV

--- src/main/java/Lab4_MySQL/App.java
+++ src/main/java/Lab4_MySQL/App.java
@@ -1,18 +1,25 @@
 package Lab4_MySQL;
 
-import java.sql.DriverManager;
-import java.sql.*;
+import Lab1_CSV.User;
 
-public class App {
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class App{
 
     public static void main(String[] args) {
+        User u = new User();
+        List<User> users = new ArrayList<>();
+
         try {
             Connection conn = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/univ",
                     "root", "sport1337");
 
             Statement stmt = conn.createStatement();
-            stmt.executeUpdate("DELETE FROM user WHERE id = 2;");
-            stmt.executeUpdate("INSERT INTO user(id, fio, phone) VALUE(3, 'T', '123')");
+
+//            stmt.executeUpdate("DELETE FROM user WHERE id = 2;");
+//            stmt.executeUpdate("INSERT INTO user(id, fio, phone) VALUE(3, 'T', '123')");
 
             ResultSet rs = stmt.executeQuery("SELECT* FROM user;");
 
@@ -20,7 +27,32 @@ public class App {
                 int id = rs.getInt("id");
                 String fio = rs.getString("fio");
                 String phone = rs.getString("phone");
+//                System.out.println(id + " " + fio + " " + phone);
+                users.add(new User(id, fio, phone));
+            }
+            u.toCSV(users);
+
+//            stmt.executeUpdate("DELETE FROM user");
+
+            users.clear();
+            users = new ArrayList<>();
+
+            users = u.fromCSV("CSV.csv", users);
+
+            for (int i = 0; i < users.size(); i++){
+                stmt.executeUpdate("INSERT INTO user(id, fio, phone) " +
+                        "VALUE(" + users.get(i).getId() + "," +
+                        users.get(i).getFio() + "," +
+                        users.get(i).getPhone() + ");");
+            }
+
+
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                String fio = rs.getString("fio");
+                String phone = rs.getString("phone");
                 System.out.println(id + " " + fio + " " + phone);
+//                users.add(new User(id, fio, phone));
             }
 
             conn.close();
