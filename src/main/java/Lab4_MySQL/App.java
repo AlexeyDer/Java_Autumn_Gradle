3944092d4b4a3f4ff7ad783ed56e8f0275d87419
AlexeyDer <hottelmich@gmail.com>
2019-11-27 12:03:29 +0700

    rename mainclass to lab4_mysql:

--- src/main/java/Lab4_MySQL/App.java
+++ src/main/java/Lab4_MySQL/App.java
@@ -7,17 +7,20 @@ public class App {
 
     public static void main(String[] args) {
         try {
-            Connection conn = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/",
+            Connection conn = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/univ",
                     "root", "sport1337");
 
             Statement stmt = conn.createStatement();
-            stmt.executeUpdate("DELETE FROM user WHERE id = 3;");
+            stmt.executeUpdate("DELETE FROM user WHERE id = 2;");
+            stmt.executeUpdate("INSERT INTO user(id, fio, phone) VALUE(3, 'T', '123')");
 
             ResultSet rs = stmt.executeQuery("SELECT* FROM user;");
 
             while (rs.next()) {
+                int id = rs.getInt("id");
                 String fio = rs.getString("fio");
-                System.out.println(fio);
+                String phone = rs.getString("phone");
+                System.out.println(id + " " + fio + " " + phone);
             }
 
             conn.close();
