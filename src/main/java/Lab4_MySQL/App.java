87e31a3cb587e3eff3ae31387d93cce06383ca69
AlexeyDer <hottelmich@gmail.com>
2020-01-03 17:01:55 +0700

    Add testing insert with and without transaction

--- src/main/java/Lab4_MySQL/App.java
+++ src/main/java/Lab4_MySQL/App.java
@@ -5,6 +5,7 @@ import Lab1_CSV.User;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class App {
 
@@ -37,43 +38,79 @@ public class App {
                     "root", "sport1337");
             Statement stmt = conn.createStatement();
 
-            stmt.executeUpdate("DELETE FROM user");
+            System.out.println("1. From CSV to DB and from DB to CSV");
+            System.out.println("2. Time insert in DB with transaction and without");
 
-            //////////////////////////////////////////////////////////////////////
-            ////////**** Из CSV в БД ****/////////
 
-            // Считываем данные из CSV файла
-            users = u.fromCSV("CSV.csv", users);
+            switch (new Scanner(System.in).nextInt()) {
+                case 1:
+                    stmt.executeUpdate("DELETE FROM user");
 
-            // Проверка на существетвание таблицы
-            stmt.execute("CREATE TABLE IF NOT EXISTS user(id INT, fio VARCHAR(24), phone VARCHAR(12))");
+                    //////////////////////////////////////////////////////////////////////
+                    ////////**** Из CSV в БД ****/////////
 
-            // Добавление в БД
-            for (int i = 0; i < users.size(); i++) {
-                stmt.executeUpdate("INSERT INTO user(id, fio, phone) VALUE(" + users.get(i).getId() + "," +
-                        "\"" + users.get(i).getFio()+ "\"" + "," + users.get(i).getPhone() + ");");
+                    // Считываем данные из CSV файла
+                    users = u.fromCSV("CSV.csv", users);
+
+                    // Проверка на существетвание таблицы
+                    stmt.execute("CREATE TABLE IF NOT EXISTS user(id INT, fio VARCHAR(24), phone VARCHAR(12))");
+
+                    // Добавление в БД
+                    for (int i = 0; i < users.size(); i++) {
+                        stmt.executeUpdate("INSERT INTO user(id, fio, phone) VALUE(" + users.get(i).getId() + "," +
+                                "\"" + users.get(i).getFio() + "\"" + "," + users.get(i).getPhone() + ");");
+                    }
+
+                    app.print(users);
+                    users.clear();
+
+                    ////////////////////////////////////////////////////////////////////////
+                    //////////**** Из БД в CSV ****/////////
+
+                    users = new ArrayList<>();
+
+                    // Считываем таблицу
+                    ResultSet rs = stmt.executeQuery("SELECT* FROM user;");
+                    users = app.read(rs, users);
+                    u.toCSV(users);
+                    rs.close();
+                    break;
+                case 2:
+                    stmt.executeUpdate("DELETE FROM testTime");
+
+                    long a = System.nanoTime();
+                    // Добавление в БД
+                    for (int i = 0; i < 1000; i++) {
+                        stmt.executeUpdate("INSERT INTO testTime(id, fio) VALUE(" + (i + 1) + ", \"Alexey Derevtsov\"" + ");");
+                    }
+                    long b = System.nanoTime();
+
+                    System.out.println("Время добавления 1000 элементов без транзакций в БД\nСекунды: " + (b - a)/1000000000.0);
+
+                    conn.setAutoCommit(false);
+
+                    long start = System.nanoTime();
+                    // Добавление в БД
+                    for (int i = 0; i < 1000; i++) {
+                        stmt = conn.createStatement();
+                        stmt.executeUpdate("INSERT INTO testTime(id, fio) VALUE(" + (i + 1) + ", \"Alexey Derevtsov\"" + ");");
+                    }
+                    conn.commit();
+                    long end = System.nanoTime();
+
+                    System.out.println("Время добавления 1000 элементов c транзакциями в БД\nСекунды: " + (end - start)/1000000000.0);
+
+
+                    break;
             }
-
-            app.print(users);
-            users.clear();
-            
-            ////////////////////////////////////////////////////////////////////////
-            //////////**** Из БД в CSV ****/////////
-
-            users = new ArrayList<>();
-
-            // Считываем таблицу
-            ResultSet rs = stmt.executeQuery("SELECT* FROM user;");
-            users = app.read(rs, users);
-            u.toCSV(users);
-
 
             conn.close();
             stmt.close();
-            rs.close();
+
 
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
+
 }
--- src/main/java/Lab8/Reflection/ExampleOnLesson.java
+++
@@ -1,7 +0,0 @@
-package Lab8.Reflection;
-
-public class ExampleOnLesson {
-    public static void main(String[] args) {
-
-    }
-}
